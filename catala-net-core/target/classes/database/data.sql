43f4bcfbfcc74b7c5445b8fc86b9c4a4a7ab88ec
romrosmay <99548008+romrosmay@users.noreply.github.com>
2023-10-06 07:16:02 +0200

    Apply .gitignore

--- catala-net-core/target/classes/database/data.sql
+++
@@ -1,9 +0,0 @@
--- Insertem les dades de les consultes (queries)
-INSERT INTO T_QUERY (QUERY_TEXT, ACTIVE)
-VALUES ('augmentar brillantor apple', true);
-
-INSERT INTO T_QUERY (QUERY_TEXT, ACTIVE)
-VALUES ('Barcelona', true);
-
-INSERT INTO T_QUERY (QUERY_TEXT, ACTIVE)
-VALUES ('biografia Gerard Romero', true);
